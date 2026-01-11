baf3a4ed500bb30e8720ba4afb473bcc78507dfb
Peter Westerbaan <pwesterbaan2@gmail.com>
2026-01-10 21:15:41 -0500

    Fixed incorrectly sorted page numbers. Changed logic for saving pdf

--- getPdfPages.sh
+++ getPdfPages.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # script used to feed numbers into pdftk based on section name of notes
-#TODO: auto complete in .bashrc
-#TODO: make help message
-
+#TODO: (multiple) auto complete in .bashrc
 
 toc="Table Of Contents"  # Ignore page numbers for TOC
 
@@ -17,19 +15,37 @@ pattern=$1
 fname=$2
 output=$3
 
+#TODO: make help message
+#https://www.howtogeek.com/beginner-friendly-ways-to-level-up-your-bash-scripts/
+_help() {
+  cat <<EOF
+Usage: $(basename "$0") [OPTIONS] \"pattern\" \"fname\" \"output\"
+
+OPTIONS
+  -h     display this help menu
+  -r     do NOT press this button
+  -a     echo something
+EOF
+}
+
 if [ "$#" -ne 3 ]; then
     echo Args: \"pattern\" \"fname\" \"output\"
-    if [ "$#" -eq 2 ]; then
-	# Print matching page range
-	pages=$(comm -23 \
-            <(pdfgrep --page-number --no-filename --cache --fixed-strings "$pattern" $fname | cut -d: -f1 | sort -u) \
-            <(pdfgrep --page-number --no-filename --cache --fixed-strings "$toc" $fname | cut -d: -f1 | sort -u ))
-	echo $(echo "$pages" | head -n1)-$(echo "$pages" | tail -n1)
-    fi
-else
-    # Find pages in $fname containing $pattern, but not containing $toc
-    # use the matching, unique, pages in pdftk to create the file $output
-    pdftk $fname cat $(comm -23 \
-        <(pdfgrep --page-number --no-filename --cache --fixed-strings "$pattern" $fname | cut -d: -f1 | sort -u) \
-        <(pdfgrep --page-number --no-filename --cache --fixed-strings "$toc" $fname | cut -d: -f1 | sort -u ) | tr '\n' ' ') output $output
+fi
+
+# Print matching page range
+pages=$(comm -23 --nocheck-order \
+             <(pdfgrep --page-number --no-filename --cache --fixed-strings "$pattern" $fname | cut -d: -f1 | sort --numeric --unique) \
+             <(pdfgrep --page-number --no-filename --cache --fixed-strings "$toc" $fname | cut -d: -f1 | sort --unique ))
+
+# Check if $output undefined
+if [ ! -n "$output" ]; then
+    #TODO: Autocomplete for filenames
+    echo $(echo "$pages" | head -n1)-$(echo "$pages" | tail -n1)
+    read -p "Specify output file? " output
+fi
+
+# Check if $output defined now
+if [ -n "$output" ]; then
+    # echo output: $output
+    pdftk $fname cat $pages output $output
 fi
