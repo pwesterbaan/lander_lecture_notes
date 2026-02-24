6071f4c5eff417361a593940cb0611d6035566d8
Peter Westerbaan <pwesterbaan2@gmail.com>
2026-02-24 11:51:24 -0500

    Modified to handle empty page ranges. Working on flag handling

--- getPdfPages.sh
+++ getPdfPages.sh
@@ -4,11 +4,6 @@
 
 toc="Table Of Contents"  # Ignore page numbers for TOC
 
-#TODO: Unnecessary when checking $#
-# Default to all pdfs
-# fname=${2:- *.pdf}
-# echo fname: $fname
-
 # args: pattern filename output
 # eg:   ./getPdfPages.sh 6.5: math123Notes.pdf math123_NoteKeys/math123Notes_6p5.pdf
 pattern=$1
@@ -23,29 +18,41 @@ Usage: $(basename "$0") [OPTIONS] \"pattern\" \"fname\" \"output\"
 
 OPTIONS
   -h     display this help menu
-  -r     do NOT press this button
-  -a     echo something
 EOF
 }
 
-if [ "$#" -ne 3 ]; then
-    echo Args: \"pattern\" \"fname\" \"output\"
-fi
+while getopts "hd" opt; do
+    case "$opt" in
+	h)
+	    _help
+	    exit 0
+	    ;;
+	d)
+	    set -x #enable debug
+	    ;;
+    esac
+done
+#TODO remove flags from position variables
 
 # Print matching page range
 pages=$(comm -23 --nocheck-order \
              <(pdfgrep --page-number --no-filename --cache --fixed-strings "$pattern" $fname | cut -d: -f1 | sort --numeric --unique) \
              <(pdfgrep --page-number --no-filename --cache --fixed-strings "$toc" $fname | cut -d: -f1 | sort --unique ))
 
-# Check if $output undefined
-if [ ! -n "$output" ]; then
-    #TODO: Autocomplete for filenames
-    echo $(echo "$pages" | head -n1)-$(echo "$pages" | tail -n1)
-    read -p "Specify output file? " output
-fi
+# check if $pages is empty
+if [ ! -z "$pages" ]; then
+    # Check if $output undefined
+    if [ ! -n "$output" ]; then
+	#TODO: Autocomplete for filenames
+	echo $(echo "$pages" | head -n1)-$(echo "$pages" | tail -n1)
+	read -p "Specify output file? " output
+    fi
 
-# Check if $output defined now
-if [ -n "$output" ]; then
-    # echo output: $output
-    pdftk $fname cat $pages output $output
+    # Check if $output defined now
+    if [ -n "$output" ]; then
+	# echo output: $output
+	pdftk $fname cat $pages output $output
+    fi
+else
+    echo "Empty page range!"
 fi
